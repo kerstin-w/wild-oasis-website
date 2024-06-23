945be89362927de7e63383232b423ecb57f377bd
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-23 17:53:40 +0200

    feat: add NotFound component for 404 page
    - Implemented NotFound component to display a message and link to the homepage when a page is not found (404).
    - Used Link component from Next.js for client-side navigation.
    - Styled component for improved readability and user interaction.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6aca8ae84bc11b1a")
+/******/ 		__webpack_require__.h = () => ("4905dd56a0352b5b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89432b0d679473c3"; }
+/******/ 		__webpack_require__.h = function() { return "2695454e1ae73ae5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

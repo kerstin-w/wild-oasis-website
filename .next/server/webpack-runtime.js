e071b8150b0d516e4256b9872dec67aac78f56f8
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-23 10:06:26 +0200

    style: Add dropshadow to h1 on homepage

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c443c0784f2e6b56")
+/******/ 		__webpack_require__.h = () => ("19f26077d4628d6f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "364e8b89805a913b"; }
+/******/ 		__webpack_require__.h = function() { return "29327ed3a75515ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

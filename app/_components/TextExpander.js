9d216b668965df0a05a97f3b1c3355a14d7dc4dc
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-25 20:06:10 +0200

    feat: add TextExpander component for expandable text display
    - Implemented TextExpander component to allow expandable and collapsible text display
    - Uses useState to manage expanded state
    - Displays first 40 words of text by default with a Show more button
    - Expands to show full text and changes button to Show less when clicked

---
+++ app/_components/TextExpander.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+/**
+ * The TextExpander function in JavaScript truncates text to a specified length and provides a "Show more" button to expand the text.
+ * @returns The TextExpander component is being returned. It displays the text content passed as children, either in full or truncated with an option to expand and collapse the text.
+ */
+function TextExpander({ children }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const displayText = isExpanded
+    ? children
+    : children.split(' ').slice(0, 40).join(' ') + '...';
+
+  return (
+    <span>
+      {displayText}{' '}
+      <button
+        className="text-primary-700 border-b border-primary-700 leading-3 pb-1"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {isExpanded ? 'Show less' : 'Show more'}
+      </button>
+    </span>
+  );
+}
+
+export default TextExpander;

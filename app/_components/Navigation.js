e9f3eddcd7aabff2149c87c6098be46e9ee0f84d
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-26 20:53:59 +0200

    feat: Get auth sessiond data and display avatar incase user is signed in with google

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { auth } from '../_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +24,26 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-accent-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link href="/account" className="flex items-center gap-4">
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className="h-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+              <span className="hover:text-accent-400 transition-colors relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-accent-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                Guest area
+              </span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-accent-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>

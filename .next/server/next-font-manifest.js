0a0bd557704ab139180b105475b8d088dcd33ba9
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-26 20:42:43 +0200

    feat: Configure NextAuth with Google provider
    - Integrated NextAuth with Google provider for authentication.
    - Configured clientId and clientSecret from environment variables.
    - Exported auth object and GET/POST handlers from NextAuth configured with Google provider.
    - Added import for GET and POST handlers from '@/app/_lib/auth' for extended functionality.

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kerst1/Documents/Udemy/REACT/wild-oasis-website/app/layout\":[\"static/media/a3a474206a856479-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"

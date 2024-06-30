2cf436a66e1b987ab0158e5804580ff1646c6f5e
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-30 18:36:05 +0200

    feat: Enhance DateSelector with reservation context and booking validation
    - Integrated useReservation hook into DateSelector component to manage and reset the selected date range.
    - Implemented displayRange logic to check if the selected range is already booked and display an appropriate range.
    - Calculated booking details including number of nights and cabin price considering regular price and discount.
    - Added date disabling functionality to prevent selection of past dates and dates that are already booked.
    
    This update enhances the DateSelector component by incorporating reservation management and booking validation, ensuring users can only select valid dates and see accurate booking details.

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 'use client';
 
-import { isWithinInterval } from 'date-fns';
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from 'date-fns';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { useReservation } from './ReservationContext';
@@ -26,13 +31,15 @@ function isAlreadyBooked(range, datesArr) {
  * The function `DateSelector` in JavaScript defines a component that allows users to select a date range for booking a cabin, displaying pricing information and an option to clear the selected range.
  * @returns The `DateSelector` component is being returned, which includes JSX elements for a date picker and pricing information display.
  */
-function DateSelector({ settings, cabin, bookedDates }) {
+function DateSelector({ settings, cabin, bookingDates }) {
   const { range, setRange, resetRange } = useReservation();
+
+  const displayRange = isAlreadyBooked(range, bookingDates) ? {} : range;
+
   // Booking details
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -43,7 +50,7 @@ function DateSelector({ settings, cabin, bookedDates }) {
         className="pt-12 place-self-center"
         mode="range"
         onSelect={setRange}
-        selected={range}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -51,6 +58,10 @@ function DateSelector({ settings, cabin, bookedDates }) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookingDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">

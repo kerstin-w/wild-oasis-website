8c3fe3c39bc8d620f976c228271747b6bda83770
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-30 20:02:56 +0200

    fix: Import for SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Josefin_Sans } from 'next/font/google';
-import { Analytics, SpeedInsights } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
